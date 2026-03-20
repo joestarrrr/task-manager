cacdab86c1f48f2cf83051be407d3f5c2897194b
venujan nagendirakumar <venujan25@outlook.com>
2026-03-20 17:00:50 +0100

    final

--- src/test/java/com/taskmanager/controller/TaskControllerTest.java
+++ src/test/java/com/taskmanager/controller/TaskControllerTest.java
@@ -1,4 +1,54 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.taskmanager.controller;
+
+import com.taskmanager.model.Task;
+import com.taskmanager.service.TaskService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TaskController.class)
+@AutoConfigureMockMvc
 class TaskControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private TaskService taskService;
+
+    @Test
+    void shouldReturnStatus200() throws Exception {
+        mockMvc.perform(get("/tasks")).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnAList() throws Exception {
+        mockMvc.perform(get("/tasks"))
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    void shouldReturnAllTasks() throws Exception {
+
+        Task mockTask = new Task("Drink water", false, "enough", 1);
+
+        when(taskService.getTasks()).thenReturn(List.of(mockTask));
+
+        mockMvc.perform(get("/tasks"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].name").value("Drink water"))
+                .andExpect(jsonPath("$[0].description").value("enough"))
+                .andExpect(jsonPath("$[0].id").value(1));
+    }
 }
--- src/test/java/com/taskmanager/service/TaskServiceTest.java
+++ src/test/java/com/taskmanager/service/TaskServiceTest.java
@@ -3,31 +3,28 @@ package com.taskmanager.service;
 import com.taskmanager.model.Task;
 import com.taskmanager.repository.TaskRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class TaskServiceTest {
 
-    @Mock
-    TaskRepository taskRepository;
+    @MockitoBean
+    private TaskRepository taskRepository;
 
-    @InjectMocks
-    TaskService taskService;
+    @Autowired
+    private TaskService taskService;
 
     @Test
-    void getTaskById() {
-
+    void shouldVerifyTaskIsSaved() {
         Task task = new Task("Drink water", "Enough water!", false);
 
         taskService.addTasks(task);
 
-       verify(taskRepository, times(1)).save(any(Task.class));
+        verify(taskRepository, times(1)).save(any(Task.class));
     }
 }
